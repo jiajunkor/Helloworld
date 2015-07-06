6dae2e4ea7e8a43e563cb2a26aa91668fc1b6c54
jiajun_kor <jiajun_kor@promote.cache-dns.local>
2015-07-06 20:22:12 +0800

    Initial Commit

---
+++ Helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Helloworld
+//
+//  Created by jiajun_kor on 15/7/6.
+//  Copyright (c) 2015年 jiajun_kor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Helloworld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Helloworld
+//
+//  Created by jiajun_kor on 15/7/6.
+//  Copyright (c) 2015年 jiajun_kor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
